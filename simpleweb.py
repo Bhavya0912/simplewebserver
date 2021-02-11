6949e063243817b81e42bee3a28f09b1334288af
Bhavya0912 <uppalapatibhavya2002@gmail.com>
2021-02-11 09:30:17 +0000

    tables are added

--- simpleweb.py
+++ simpleweb.py
@@ -1,22 +1,3 @@
-# Developing a Simple Webserver
-## AIM:
-To develop a simple webserver to serve html pages.
-
-## DESIGN STEPS:
-### Step 1: 
-HTML content creation
-### Step 2:
-Design of webserver workflow
-### Step 3:
-Implementation using Python code
-### Step 4:
-Serving the HTML pages.
-### Step 5:
-Testing the webserver
-
-## PROGRAM:
-### simpleweb.py
-```
 from http.server import HTTPServer, BaseHTTPRequestHandler
 content = """
 <!DOCTYPE html>
@@ -51,16 +32,3 @@ server_address = ('',80)
 httpd = HTTPServer(server_address,myhandler)
 print("my webserver is running...")
 httpd.serve_forever()
-
-```
-
-## OUTPUT:
-![output](./static/img/output.jpg)
-
-
-## CODE VALIDATION REPORT:
-![output](./static/img/report.jpg)
-
-
-## RESULT:
-Thus the simple webserver is developed to server html pages and is hosted in the URL http://bhavya.student.saveetha.in/. HTML code is validated.
